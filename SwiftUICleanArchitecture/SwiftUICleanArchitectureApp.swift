fcf00a8197079d1595720ee0d8344c1187539b6b
shuro <shuro0530@gmail.com>
2024-03-27 16:25:12 +0900

    Initial Commit

---
+++ SwiftUICleanArchitecture/SwiftUICleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureApp.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Shuraw on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
